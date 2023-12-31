ed3592d58aae625bb602eca80e4c73e780eea071
Brian Clements <brian.l.clements03@gmailcom>
2023-12-31 16:32:08 -0600

    update the wrange ipynb and transfer to a .py file, and set the EDA file up nicely for EDA

--- wrangle.py
+++ wrangle.py
@@ -28,8 +28,59 @@ def acquire_fraud(zip_file_name_in_quotes):
     return df_test, df_train
 
 
-# In[ ]:
+# In[3]:
 
 
+def prep(df):
+    '''
+    Apply some clean and prep to the df
+    '''
+    df = df.set_index(pd.to_datetime(df['trans_date_trans_time'],format= '%Y-%m-%d %H:%M:%S')).sort_index()
+    df['age'] = (df.index - pd.DatetimeIndex(df['dob']))// pd.Timedelta('365D')
+    df['dayofweek'] = df.index.day_name()
+    df['hourofday'] = df.index.hour
+    df = df.drop(columns=['Unnamed: 0','trans_date_trans_time','cc_num','first','last','street','city','state','trans_num','lat','long','dob','trans_num','unix_time'])
+    df['age_group'] = pd.cut(df['age'],[0,25,35,45,55,65,75,100], labels= ['Youth','Young_Adult','Adult','Early_Mid_Age','Mid_Age','Retirement_Age','Older_Person'],right=False)
+    
+    return df
 
+
+# In[4]:
+
+
+def get_target_and_features(df):
+    '''
+    A UDF to define the target variable and the rest
+    '''
+    target = df.columns.to_list()[9]
+    features = df.columns[df.columns != target].to_list()
+    return target,features
+
+
+# In[5]:
+
+
+def train_val(df):
+    '''
+    A UDF to split the train data set into train and validate (both for x and y variables)
+    '''
+    target, features = get_target_and_features(df)
+    y = df[target]
+    x = df[features]
+    x_train, x_validate, y_train, y_validate = train_test_split(x,y,test_size=.30, random_state=42)
+    return x_train,x_validate,y_train,y_validate
+
+
+# In[6]:
+
+
+def test_df_x_y_split(df):
+    '''
+    A UDF to define the target and featurs and define the X and Y dataframes for the TEST dataset
+    '''
+    target, features = get_target_and_features(df)
+    x_test = df[features]
+    y_test = df[target]
+    
+    return x_test,y_test
 
